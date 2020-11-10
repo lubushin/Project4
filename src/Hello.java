e826d3fe093d00d6986281a4e43e4e1149f1ce06
Lyubov Ubushin <Lyubau1124@gmail.com>
2020-11-10 12:49:12 -0500

    Project is shared in remote repo

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
         System.out.println("Hello5");
         //new stuff
         // mac users are happy to see commit
+        // I have a;ready published on GitHub
+        // Absolutely NO
     }
 }
